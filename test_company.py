79b2069721829d7e81e98dbb85532cdf988d6494
Karen Palacio <karen.palacio.1994@gmail.com>
2024-05-24 18:24:16 -0300

    add comments to test cases to add readability

--- test_company.py
+++ test_company.py
@@ -2,22 +2,27 @@ from datetime import datetime
 from main import Client, Company
 
 def test_deliver_package():
+    # Create a test Client and Company
     farmacia = Client("farmacia Lider")
     AA = Company("aerolineas argentinas")
 
+    # Deliver a package from A to B
     AA.deliver_package(farmacia, "cordoba", "baires", "sertal")
     assert len(AA.deliveries) == 1
     assert len(AA.clients) == 1
-    assert AA.transactions_log == {'{}'.format(datetime.today().strftime('%Y-%m-%d')): 10}
+    assert AA.deliveries_log == {'{}'.format(datetime.today().strftime('%Y-%m-%d')): 10}
 
+    # Deliver another package from A to B
     AA.deliver_package(farmacia, "cordoba", "baires", "actron")
     assert len(AA.deliveries) == 2
-    assert AA.transactions_log == {'{}'.format(datetime.today().strftime('%Y-%m-%d')): 20}
+    assert AA.deliveries_log == {'{}'.format(datetime.today().strftime('%Y-%m-%d')): 20}
 
 def test_transaction_report_by_date():
+    # Create a test Client and Company
     farmacia = Client("farmacia Lider")
     AA = Company("aerolineas argentinas")
 
+    # Deliver two packages
     AA.deliver_package(farmacia, "cordoba", "baires", "sertales")
     AA.deliver_package(farmacia, "cordoba", "baires", "actron")
 
