d8911f5bca711cca9f2a13f75b962b420b27962d
Karen Palacio <karen.palacio.1994@gmail.com>
2024-05-25 11:08:16 -0300

    use black to format test file according to pep8

--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from main import Client, DeliveryCompany, Location
 
+
 def test_deliver_package():
     # Create a test Client and Company
     farmacia = Client("farmacia Lider")
@@ -12,14 +13,24 @@ def test_deliver_package():
     delivery_a = AA.deliver_package(farmacia, cordoba, baires, "sertal")
     assert len(AA.deliveries) == 1
     assert len(AA.clients) == 1
-    assert AA.deliveries_log[datetime.today().strftime('%Y-%m-%d')]["total_revenue"] == 10
-    assert AA.deliveries_log[datetime.today().strftime('%Y-%m-%d')]["deliveries"] == [delivery_a]
+    assert (
+        AA.deliveries_log[datetime.today().strftime("%Y-%m-%d")]["total_revenue"] == 10
+    )
+    assert AA.deliveries_log[datetime.today().strftime("%Y-%m-%d")]["deliveries"] == [
+        delivery_a
+    ]
 
     # Deliver another package from A to B
     delivery_b = AA.deliver_package(farmacia, cordoba, baires, "actron")
     assert len(AA.deliveries) == 2
-    assert AA.deliveries_log[datetime.today().strftime('%Y-%m-%d')]["total_revenue"] == 20
-    assert AA.deliveries_log[datetime.today().strftime('%Y-%m-%d')]["deliveries"] == [delivery_a, delivery_b]
+    assert (
+        AA.deliveries_log[datetime.today().strftime("%Y-%m-%d")]["total_revenue"] == 20
+    )
+    assert AA.deliveries_log[datetime.today().strftime("%Y-%m-%d")]["deliveries"] == [
+        delivery_a,
+        delivery_b,
+    ]
+
 
 def test_transaction_report_by_date():
     # Create a test Client and Company
@@ -32,11 +43,15 @@ def test_transaction_report_by_date():
     AA.deliver_package(farmacia, cordoba, baires, "sertales")
     AA.deliver_package(farmacia, cordoba, baires, "actron")
 
-    report = AA.report_by_date(date= datetime.today().strftime('%Y-%m-%d'))
+    report = AA.report_by_date(date=datetime.today().strftime("%Y-%m-%d"))
     assert isinstance(report, dict)
     assert len(report) == 1
-    assert '{}'.format(datetime.today().strftime('%Y-%m-%d')) in report
-    assert report['{}'.format(datetime.today().strftime('%Y-%m-%d'))]["total_revenue"] == 20
+    assert "{}".format(datetime.today().strftime("%Y-%m-%d")) in report
+    assert (
+        report["{}".format(datetime.today().strftime("%Y-%m-%d"))]["total_revenue"]
+        == 20
+    )
+
 
 def test_transaction_report_by_date_no_deliveries():
 
@@ -51,8 +66,9 @@ def test_transaction_report_by_date_no_deliveries():
     AA.deliver_package(farmacia, cordoba, baires, "actron")
 
     # Date with no deliveries
-    report = AA.report_by_date(date='2023-05-24')
+    report = AA.report_by_date(date="2023-05-24")
     assert report == ""
+
 
 def test_transaction_report_by_invalid_date_format():
     # Create a test Client and Company
@@ -66,5 +82,5 @@ def test_transaction_report_by_invalid_date_format():
     AA.deliver_package(farmacia, cordoba, baires, "actron")
 
     # Invalid date format
-    report = AA.report_by_date(date='2023/05/24')
+    report = AA.report_by_date(date="2023/05/24")
     assert report == ""
